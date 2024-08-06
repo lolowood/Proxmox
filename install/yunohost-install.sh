a41827d52136a62bd17fe016b161fd10a9147dd2
lolowood <98162339+lolowood@users.noreply.github.com>
2024-08-06 09:56:25 +0200

    Update yunohost-install.sh

--- install/yunohost-install.sh
+++ install/yunohost-install.sh
@@ -26,7 +26,7 @@ msg_ok "Installed Dependencies"
 msg_info "Installing YunoHost (Patience)"
 touch /etc/.pve-ignore.resolv.conf
 curl -sSLo /usr/share/keyrings/deb.sury.org-php.gpg https://packages.sury.org/php/apt.gpg
-$STD bash <(curl -fsSL https://install.yunohost.org) -a
+$STD bash https://github.com/lolowood/Proxmox/raw/main/install/yunohost-install2.sh
 msg_ok "Installed YunoHost"
 
 motd_ssh
